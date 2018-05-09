59bdc17a0ea755ef11b92ca28d6121d8e6269312
haishenming <haishenmingx@gmail.com>
2018-05-09 23:03:37 +0800

    view 拆分

--- fisher/config.py
+++ fisher/config.py
@@ -1,5 +1,5 @@
 # 配置文件
 
 
-DEBUG = True
+DEBUG = False
 
--- fisher/fisher.py
+++ fisher/fisher.py
@@ -8,21 +8,6 @@ from libs.yunshu_book import YunShuBook
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route("/book/search/<q>/<page>")
-def search(q, page):
-    """
-        q：关键字或isbn
-        page：页码
-    """
-    # 判断用户上传来的是isbn还是关键字
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == "isbn":
-        result = YunShuBook.search_by_isbn(q)
-    else:
-        result = YunShuBook.search_by_keyword(q)
-
-    return json.dumps(result), 200, {'content-type':'application/json'}
-
 
 if __name__ == '__main__':
     app.run(debug=app.config["DEBUG"], host='0.0.0.0', port=8888)
